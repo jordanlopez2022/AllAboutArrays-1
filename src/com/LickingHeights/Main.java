06b57681cc55406c33557f9a8579476bb7e9d0a2
JORDAN LOPEZ <jordan.lopez.2022@student.lhschools.org>
2020-05-14 13:09:43 -0400

    Pattern2 Completed

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -12,6 +12,7 @@ public class Main {
 //        int[] array = createNumberArray(size, lowestNumber, highestNumber);
 
         Pattern1();
+        Pattern2();
         
     }
 
@@ -43,6 +44,21 @@ public class Main {
 
             for(int columns = 0; columns < 6; columns++){
                 System.out.print("*");
+            }
+
+            System.out.println();
+
+
+        }
+
+
+
+    }
+    public static void Pattern2(){
+        for(int rows=1; rows < 7; rows++){
+
+            for(int columns = 1; columns < 7; columns++){
+                System.out.print(rows);
             }
 
             System.out.println();
